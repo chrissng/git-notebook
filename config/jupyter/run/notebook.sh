a1085e3df5e7ea340b1c30c0155fec8b4105e2aa
Chris <chris@data.gov.sg>
2016-10-10 19:44:45 -0700

    fetch the latest changes from repo before starting notebook server

--- config/jupyter/run/notebook.sh
+++ config/jupyter/run/notebook.sh
@@ -10,5 +10,10 @@ chmod 0400 /root/.ssh/id_rsa
 git config --global user.name  $GIT_USER && \
 git config --global user.email $GIT_EMAIL
 
+# fetch the latest changes from repo
+pushd $ABSOLUTE_BASE_DIR/$GIT_REPO_NAME
+git fetch
+popd
+
 # start notebook with extension activated
 jupyter notebook --ip=*  --NotebookApp.server_extensions="['github_commit_push']"
